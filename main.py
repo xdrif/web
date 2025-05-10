148203f97c396e09d0b76d8b60352e91a1feb071
Johnley Dessoit <Dessoitjhon@gmail.com>
2025-05-10 19:11:30 -0400

    modified main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
@@ -13,5 +14,5 @@ def about():
 @app.route('/projects')  # <-- Add the @ symbol here
 def projects():
     return render_template('projects.html')
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
